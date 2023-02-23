be90e04ed7ffb465b7b8174e86e831107349115a
paolajpco <paolajpco@gmail.com>
2023-02-22 22:52:53 -0500

    Corrigiendo errores con cucumber

--- src/test/java/Runners/PostDataUsersTests.java
+++ src/test/java/Runners/PostDataUsersTests.java
@@ -15,7 +15,7 @@ import java.util.Random;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SerenityRunner.class)
-public class PostDataUsersTests  {
+public class PostDataUsersTests {
     private static final String restApiUrl = "https://dummyapi.io";
 
     @Test
