6b54cb2f71744c1b5ea348c860101a1a6d2d649e
paolajpco <paolajpco@gmail.com>
2023-02-22 20:20:00 -0500

    Avance steps definitions

--- src/test/java/StepDefinitions/GetUsersSteps.java
+++ src/test/java/StepDefinitions/GetUsersSteps.java
@@ -3,7 +3,36 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import tasks.GetDataUsers;
 
-public class  GetUsersSteps {
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class GetUsersSteps {
+    private static final String restApiUrl = "https://dummyapi.io";
+    @Given("{actor} need to know the available users")
+    public void callApiEndpoint(Actor actor) {
+        //actor = Actor.named("Paola user").whoCan(CallAnApi.at(restApiUrl));
+        theActorCalled("Paola user").whoCan(CallAnApi.at(restApiUrl));
+    }
+    @When("{actor] get the response from the GET method ")
+    public void requestHeader(Actor actor) {
+        //actor.attemptsTo(new GetDataUsers());
+        theActorInTheSpotlight().attemptsTo(new GetDataUsers());
+    }
+
+    @Then("I get a valid HTTP response code 200")
+    public void getValidResponse(){
+        //assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
+        assertThat(lastResponse().statusCode()).isEqualTo(200);
+
+    }
+
+
 
 }
--- target/test-classes/features/GetUsers.feature
+++ target/test-classes/features/GetUsers.feature
@@ -1,10 +1,9 @@
 Feature: Test Get method in Sample Users REST API testing
 
   Scenario: Get data Users
-    Given Paola set GET users service api endpoint
-    When I set request HEADER
-    And Send GET HTTP request
-    Then I receive valid HTTP response code 200
+    Given I need to know the available users
+    When I get the response from the GET method
+    Then I get a valid HTTP response code 200
 
   Scenario: Get data Users invalid
     Given Paola set GET users service api endpoint
