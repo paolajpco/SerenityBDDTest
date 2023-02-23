90f2f9fb85e74c58edd573ebad28d9208196b1cd
paolajpco <paolajpco@gmail.com>
2023-02-22 21:06:34 -0500

    Corrigiendo errores con cucumber

--- src/test/java/StepDefinitions/GetUsersSteps.java
+++ src/test/java/StepDefinitions/GetUsersSteps.java
@@ -13,23 +13,27 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+
 public class GetUsersSteps {
     private static final String restApiUrl = "https://dummyapi.io";
-    @Given("{actor} need to know the available users")
-    public void callApiEndpoint(Actor actor) {
+    @Given("I need to know the available users")
+    public void i_need_to_know_the_available_users() {
         //actor = Actor.named("Paola user").whoCan(CallAnApi.at(restApiUrl));
         theActorCalled("Paola user").whoCan(CallAnApi.at(restApiUrl));
+        throw new io.cucumber.java.PendingException();
     }
-    @When("{actor] get the response from the GET method ")
-    public void requestHeader(Actor actor) {
+    @When("I get the response from the GET method")
+    public void i_get_the_response_from_the_get_method() {
         //actor.attemptsTo(new GetDataUsers());
         theActorInTheSpotlight().attemptsTo(new GetDataUsers());
+        throw new io.cucumber.java.PendingException();
     }
 
-    @Then("I get a valid HTTP response code 200")
-    public void getValidResponse(){
+    @Then("I get a valid HTTP response code {int}")
+    public void i_get_a_valid_http_response_code(Integer int1) {
         //assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
         assertThat(lastResponse().statusCode()).isEqualTo(200);
+        throw new io.cucumber.java.PendingException();
 
     }
 
